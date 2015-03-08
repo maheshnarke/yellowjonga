1a4990718132ca39759b6953db168006ad753574
maheshnarke <narkemahesh@yahoo.com>
2015-03-08 20:55:50 +0530

    test

---
+++ index.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">    
+  <title>Loot On Sale</title><!-- This is what you see on your browser tab-->
+
+  <link rel="icon" href="img/favicon.png">
+  <!-- web page icon on the Apple iPhone, iPod Touch, and iPad. -->
+  <link rel="apple-touch-icon" sizes="144x144" href="img/apple-touch-icon-ipad-retina.png" />
+  <link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-iphone-retina.png" />
+  <link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-ipad.png" />
+  <link rel="apple-touch-icon" sizes="57x57" href="img/apple-touch-icon-iphone.png" />
+
+  <!-- BootStrap link -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+
+  <link rel="stylesheet" href="css/style.css">
+  <!-- ICONS: -->
+  <link rel="stylesheet" href="css/simple-line-icons.css">    
+
+  <!--GOOGLE FONTS-->    
+  <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Antic|Raleway:300">
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="js/bootstrap.min.js"></script>
+
+  <!--Other necessary scripts-->
+</head>
+<body data-spy="scroll">
+  <header>   
+     <!-- ===========================
+        HERO AREA
+        =========================== -->
+        <div id="hero">           
+          <!-- Featured image on the Hero area -->
+          <img class="heroshot wow bounceInUp"  src="img/woodshelves.png" alt="Featured Work">            
+        </div><!--HERO AREA END-->        
+
+        <!-- ===========================
+         NAVBAR START
+         =========================== -->
+         <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+           <div class="container">
+
+            <div class="navbar-header">
+             <a class="navbar-brand" href="#">
+              <!-- Replace Drifolio Bootstrap with your Site Name and remove icon-grid to remove the placeholder icon -->
+              <span class="brandicon icon-grid"></span>
+              <span class="brandname">Loot On Sale</span>
+              <p><span class="brandname_tagline">Dont just buy,buy it on sale </span></p><!-- or Cost next to nothing -->
+
+            </a>
+          </div>
+
+          <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav navbar-right"><!--YOUR NAVIGATION ITEMS STRAT BELOW-->
+              <li><a href="#"><span class="btnicon icon-user"></span>Login</a></li>
+              <li><a href="#"><span class="btnicon icon-user"></span>Signup</a></li>
+            </ul>
+
+          </div><!--.nav-collapse -->
+        </div>
+      </nav><!--navbar end-->        
+    </header><!--header end-->     
+
+    <div id="clients">
+     <div class="container">
+       <div class="col-md-1">
+         <h4>Categories:</h4>
+       </div>
+       <div class="col-md-11">
+         <ul><!--CLIENTS LOGO-->
+           <li> <img src="img/amazon.png" alt="Amazon"></li>
+           <li> <img src="img/amazon.png" alt="Amazon"></li>
+           <li><img src="img/elance-odesk.png" alt="Elance-oDesk"></li>
+           <li><img src="img/curb.png" alt="Curb Envy"></li>                     
+           <li><img src="img/curb.png" alt="Curb Envy"></li>  
+         </ul><!--CLIENTS LOGO END-->
+       </div>
+     </div>
+     <hr><!-- SECTION SEPARETOR LINE -->
+   </div><!--FEATURED CLIENTS SECTION END-->
+
+
+
+    <!-- ===========================
+    ABOUT SECTION START
+    =========================== -->
+    <div id="about" class="container">
+
+      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
+      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
+      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
+      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
+      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.     
+      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
+      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
+    </div><!-- ABOUT SECTION END -->
+     <div id="about" class="container">
+
+      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
+      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
+      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
+      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
+      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.     
+      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
+      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
+    </div>
+     <div id="about" class="container">
+
+      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
+      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
+      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
+      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
+      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.     
+      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
+      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
+    </div>
+     <div id="about" class="container">
+
+      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
+      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
+      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
+      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
+      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.     
+      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
+      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
+    </div>
+    
+    <!-- SECTION SEPARETOR LINE -->
+    <!-- ===========================
+    FOOTER START-->
+   <footer>
+        <div class="container">
+
+            <div class="footerlinks"><!-- FOOTER LINKS START -->            
+              <span class="bigicon icon-speedometer "></span>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Terms & Condition</a></li>
+                    <li><a href="#">Contact</a></li>                   
+                </ul>
+            </div><!-- FOOTER LINKS END -->
+             
+            <div class="footersocial wow fadeInUp" data-wow-duration="3s"><!-- FOOTER SOCIAL ICONS START -->
+              <h4>we are social</h4>
+                <ul>
+                    <li><a href="http://facebook.com"><span class="icon-social-facebook"></span></a></li>
+                    <li><a href="http://twitter.com"><span class="icon-social-twitter"></span></a></li>
+                    <li><a href="http://youtube.com"><span class="icon-social-youtube"></span></a></li>
+                 </ul>
+             </div><!-- FOOTER SOCIAL ICONS END -->
+         </div>
+     </footer><!-- FOOTER END -->
+
+
+</body>
+</html>

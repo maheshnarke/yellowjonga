5fb92543d8773e7abcd74a60a9b3814732403b63
maheshnarke <narkemahesh@yahoo.com>
2015-03-08 23:43:48 +0530

    index file update

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">    
-  <title>Loot On Sale</title><!-- This is what you see on your browser tab-->
+  <title>Loot On Sale</title>
 
   <link rel="icon" href="img/favicon.png">
   <!-- web page icon on the Apple iPhone, iPod Touch, and iPad. -->
@@ -13,159 +13,139 @@
   <link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-ipad.png" />
   <link rel="apple-touch-icon" sizes="57x57" href="img/apple-touch-icon-iphone.png" />
 
-  <!-- BootStrap link -->
+  <!--link--> 
   <link rel="stylesheet" href="css/bootstrap.min.css">
-
   <link rel="stylesheet" href="css/style.css">
-  <!-- ICONS: -->
+  <link rel="stylesheet" href="css/responsive.css">
   <link rel="stylesheet" href="css/simple-line-icons.css">    
-
-  <!--GOOGLE FONTS-->    
   <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Antic|Raleway:300">
 
-  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-  <script src="http://code.jquery.com/jquery-latest.min.js"></script>
 
-  <!-- Include all compiled plugins (below), or include individual files as needed -->
-  <script src="js/bootstrap.min.js"></script>
-  
 </head>
 <body data-spy="scroll">
   <header>   
-     <!-- ===========================
-        HERO AREA
-        =========================== -->
-        <div id="hero">           
-          <!-- Featured image on the Hero area -->
-          <img class="heroshot wow bounceInUp"  src="img/1.png"  id="img1">  
-          <img class="heroshot wow bounceInUp"  src="img/2.png"  id="img2">  
-          <img class="heroshot wow bounceInUp"  src="img/3.png"  id="img3">            
-          <img class="heroshot wow bounceInUp"  src="img/4.png"  id="img4">  
-          <img class="heroshot wow bounceInUp"  src="img/5.png"  id="img5">  
-        </div><!--HERO AREA END-->       
-        <!-- ==========================5
-         NAVBAR STAR
-         =========================== -->
-         <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-           <div class="container">
-
-            <div class="navbar-header">
-             <a class="navbar-brand" href="#">
-              <!-- Replace Drifolio Bootstrap with your Site Name and remove icon-grid to remove the placeholder icon -->
-              <span class="brandicon icon-grid"></span>
-              <span class="brandname">Loot On Sale</span>
-              <p><span class="brandname_tagline">Dont just buy,buy it on sale </span></p><!-- or Cost next to nothing -->
-
-            </a>
-          </div>
-
-          <div class="collapse navbar-collapse">
-            <ul class="nav navbar-nav navbar-right"><!--YOUR NAVIGATION ITEMS STRAT BELOW-->
-              <li><a href="#"><span class="btnicon icon-user"></span>Login</a></li>
-              <li><a href="#"><span class="btnicon icon-user"></span>Signup</a></li>
-            </ul>
-
-          </div><!--.nav-collapse -->
-        </div>
-      </nav><!--navbar end-->        
-    </header><!--header end-->     
-
-    <div id="clients">
+    <div id="hero">           
+      <img class="heroshot wow bounceInUp"  src="img/1.png"  id="img1" >  
+      <img class="heroshot wow bounceInUp"  src="img/2.png"  id="img2" style="display:none">  
+      <img class="heroshot wow bounceInUp"  src="img/3.png"  id="img3" style="display:none">            
+      <img class="heroshot wow bounceInUp"  src="img/4.png"  id="img4" style="display:none">  
+      <img class="heroshot wow bounceInUp"  src="img/5.png"  id="img5" style="display:none">  
+    </div>
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
      <div class="container">
-       <div class="col-md-1">
-         <h4>Categories:</h4>
-       </div>
-       <div class="col-md-11">
-         <ul><!--CLIENTS LOGO-->
-           <li> <img src="img/amazon.png" alt="Amazon" onmouseover="hoverEvent(1)"></li>
-           <li> <img src="img/amazon.png" alt="Amazon" onmouseover="hoverEvent(2)"></li>
-           <li><img src="img/elance-odesk.png" alt="Elance-oDesk" onmouseover="hoverEvent(3)"></li>
-           <li><img src="img/curb.png" alt="Curb Envy" onmouseover="hoverEvent(4)"></li>                     
-           <li><img src="img/curb.png" alt="Curb Envy" onmouseover="hoverEvent(5)"></li>  
-         </ul><!--CLIENTS LOGO END-->
-       </div>
-     </div>
-     <hr><!-- SECTION SEPARETOR LINE -->
-   </div><!--FEATURED CLIENTS SECTION END-->
 
-
-
-    <!-- ===========================
-    ABOUT SECTION START
-    =========================== -->
-    <div id="about" class="container">
-
-      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
-      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
-      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
-      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
-      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.     
-      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
-      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
-    </div><!-- ABOUT SECTION END -->
-     <div id="about" class="container">
-
-      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
-      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
-      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
-      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
-      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.     
-      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
-      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
+      <div class="navbar-header">
+       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+         <span class="icon-bar"></span>
+         <span class="icon-bar"></span>
+         <span class="icon-bar"></span>
+       </button>
+       <a class="navbar-brand" href="#">
+        <span class="brandicon icon-grid"></span>
+        <span class="brandname">Loot On Sale</span>
+        <p><span class="brandname_tagline">Dont just buy,buy it on sale </span></p><!-- or Cost next to nothing -->
+      </a>
     </div>
-     <div id="about" class="container">
-
-      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
-      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
-      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
-      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
-      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.     
-      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
-      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
+    <div class="collapse navbar-collapse">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="btnicon icon-user"></span>Login</a></li>
+        <li><a href="#"><span class="btnicon icon-user"></span>Signup</a></li>
+      </ul>
     </div>
-     <div id="about" class="container">
+  </div>
+</nav>
+</header>
+<div id="clients">
+ <div class="container">
+   <div class="col-md-1">
+     <h4>Categories:</h4>
+   </div>
+   <div class="col-md-11">
+     <ul>
+       <li> <img src="img/amazon.png" alt="Amazon" onmouseover="hoverEvent(1)"></li>
+       <li> <img src="img/amazon.png" alt="Amazon" onmouseover="hoverEvent(2)"></li>
+       <li><img src="img/elance-odesk.png" alt="Elance-oDesk" onmouseover="hoverEvent(3)"></li>
+       <li><img src="img/curb.png" alt="Curb Envy" onmouseover="hoverEvent(4)"></li>                     
+       <li><img src="img/curb.png" alt="Curb Envy" onmouseover="hoverEvent(5)"></li>  
+     </ul>
+   </div>
+ </div>
+ <hr>
+</div>
+<div id="about" class="container">
+why it will change the game
 
-      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
-      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
-      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
-      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
-      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.     
-      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
-      I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings? I don't care if it takes from now till the end of Shrimpfest.
+More features than rivals: Along with email, texts and phone calls, Apple says its watch will present news, health readings and other notifications in creative ways that can be read at a glance. It will have a heart rate monitor and accelerometer, and an internal motor that can signal the wearer with a subtle "tap" on the wrist. And Siri and Apple Pay will be built in. Apple is working with outside companies to create more apps; Cook has talked about using the watch as an electronic "key" for hotel doors or even cars.
+
+A powerful brand: The world's biggest tech company has a reputation for quality and a direct conduit to customers - it operates more than 400 retail stores around the world. And it has deep pockets to spend on advertising - it is showcasing the watch this month with a sleek, 12-page insert in Vogue and other fashion magazines.
+
+Apple's track record: This wouldn't be the first Apple product that revolutionized a market where rivals had struggled to break through. Other companies made digital music players before the iPod, smartphones before the iPhone and even tablets before the iPad. Most of those products failed to catch on until Apple made devices so appealing they set new standards and created new demand, said Forrester Research analyst J.P. Gownder.
+
+</div>
+<div id="about" class="container">
+hy it will change the game
+
+More features than rivals: Along with email, texts and phone calls, Apple says its watch will present news, health readings and other notifications in creative ways that can be read at a glance. It will have a heart rate monitor and accelerometer, and an internal motor that can signal the wearer with a subtle "tap" on the wrist. And Siri and Apple Pay will be built in. Apple is working with outside companies to create more apps; Cook has talked about using the watch as an electronic "key" for hotel doors or even cars.
+
+A powerful brand: The world's biggest tech company has a reputation for quality and a direct conduit to customers - it operates more than 400 retail stores around the world. And it has deep pockets to spend on advertising - it is showcasing the watch this month with a sleek, 12-page insert in Vogue and other fashion magazines.
+
+Apple's track record: This wouldn't be the first Apple product that revolutionized a market where rivals had struggled to break through. Other companies made digital music players before the iPod, smartphones before the iPhone and even tablets before the iPad. Most of those products failed to catch on until Apple made devices so appealing they set new standards and created new demand, said Forrester Research analyst J.P. Gownder.
+</div>
+<div id="about" class="container">
+
+ hy it will change the game
+
+More features than rivals: Along with email, texts and phone calls, Apple says its watch will present news, health readings and other notifications in creative ways that can be read at a glance. It will have a heart rate monitor and accelerometer, and an internal motor that can signal the wearer with a subtle "tap" on the wrist. And Siri and Apple Pay will be built in. Apple is working with outside companies to create more apps; Cook has talked about using the watch as an electronic "key" for hotel doors or even cars.
+
+A powerful brand: The world's biggest tech company has a reputation for quality and a direct conduit to customers - it operates more than 400 retail stores around the world. And it has deep pockets to spend on advertising - it is showcasing the watch this month with a sleek, 12-page insert in Vogue and other fashion magazines.
+
+Apple's track record: This wouldn't be the first Apple product that revolutionized a market where rivals had struggled to break through. Other companies made digital music players before the iPod, smartphones before the iPhone and even tablets before the iPad. Most of those products failed to catch on until Apple made devices so appealing they set new standards and created new demand, said Forrester Research analyst J.P. Gownder.
+</div>
+<div id="about" class="container">
+
+ hy it will change the game
+
+More features than rivals: Along with email, texts and phone calls, Apple says its watch will present news, health readings and other notifications in creative ways that can be read at a glance. It will have a heart rate monitor and accelerometer, and an internal motor that can signal the wearer with a subtle "tap" on the wrist. And Siri and Apple Pay will be built in. Apple is working with outside companies to create more apps; Cook has talked about using the watch as an electronic "key" for hotel doors or even cars.
+
+A powerful brand: The world's biggest tech company has a reputation for quality and a direct conduit to customers - it operates more than 400 retail stores around the world. And it has deep pockets to spend on advertising - it is showcasing the watch this month with a sleek, 12-page insert in Vogue and other fashion magazines.
+
+Apple's track record: This wouldn't be the first Apple product that revolutionized a market where rivals had struggled to break through. Other companies made digital music players before the iPod, smartphones before the iPhone and even tablets before the iPad. Most of those products failed to catch on until Apple made devices so appealing they set new standards and created new demand, said Forrester Research analyst J.P. Gownder.
+</div>
+
+<!--FOOTER START-->
+<footer>
+  <div class="container">
+
+    <div class="footerlinks">\
+      <span class="bigicon icon-speedometer "></span>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Terms & Condition</a></li>
+        <li><a href="#">Contact</a></li>                   
+      </ul>
     </div>
-    
-    <!-- SECTION SEPARETOR LINE -->
-    <!-- ===========================
-    FOOTER START-->
-   <footer>
-        <div class="container">
 
-            <div class="footerlinks"><!-- FOOTER LINKS START -->            
-              <span class="bigicon icon-speedometer "></span>
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Terms & Condition</a></li>
-                    <li><a href="#">Contact</a></li>                   
-                </ul>
-            </div><!-- FOOTER LINKS END -->
-             
-            <div class="footersocial wow fadeInUp" data-wow-duration="3s"><!-- FOOTER SOCIAL ICONS START -->
-              <h4>we are social</h4>
-                <ul>
-                    <li><a href="http://facebook.com"><span class="icon-social-facebook"></span></a></li>
-                    <li><a href="http://twitter.com"><span class="icon-social-twitter"></span></a></li>
-                    <li><a href="http://youtube.com"><span class="icon-social-youtube"></span></a></li>
-                 </ul>
-             </div><!-- FOOTER SOCIAL ICONS END -->
-         </div>
-     </footer><!-- FOOTER END -->
+    <div class="footersocial wow fadeInUp" data-wow-duration="3s">
+      <h4>we are social</h4>
+      <ul>
+        <li><a href="http://facebook.com"><span class="icon-social-facebook"></span></a></li>
+        <li><a href="http://twitter.com"><span class="icon-social-twitter"></span></a></li>
+        <li><a href="http://youtube.com"><span class="icon-social-youtube"></span></a></li>
+      </ul>
+    </div>
+  </div>
+</footer><!-- FOOTER END -->
+
+<!--<script-->
+<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
 <script type="text/javascript">
   function hoverEvent(index){
-    
+
     switch (index) {
       case 1:
-     $('#img2,#img3,#img4,#img5').hide();
+      $('#img2,#img3,#img4,#img5').hide();
       $("#img1").show();
       break;
       case 2:
@@ -185,7 +165,6 @@
       $("#img5").show();  
       break;
       default:
-      alert("default'");
       $('#img2,#img3,#img4,#img5').hide();
       $("#img1").show();
     }
